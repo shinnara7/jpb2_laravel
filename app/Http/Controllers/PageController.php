e7c0df52b61dc99c65e134d1fb70c65a4e16fab6
shinnara7 <nainglinucsy@gmail.com>
2020-09-21 00:50:05 +0630

    routes, controller and crud process for staff table

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+     public function main($key)
+    {
+    	return view('welcome',compact('key'));
+    }
+    public function home($value='')
+    {
+    	return view('myhome');
+    }
 }
---
+++ resources/views/myhome.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Home Page</title>
+</head>
+<body>
+	<h1>This is My Home Page!</h1>
+	<a href="{{route('mainpage',7)}}">Go to welcome page</a>
+</body>
+</html>
